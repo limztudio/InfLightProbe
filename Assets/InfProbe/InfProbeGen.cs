6bbed123ecbb7969a59ae64181971ff2b28339d4
LimTaewoo <limstudio@rhaon.co.kr>
2021-02-15 12:45:53 +0900

    probe finder added

--- Assets/InfProbe/InfProbeGen.cs
+++ Assets/InfProbe/InfProbeGen.cs
@@ -7,6 +7,20 @@ public struct TetIndex
 {
     public int _0, _1, _2, _3;
 };
+[System.Serializable]
+public struct TetDepth
+{
+    public byte _00;
+    public byte _01, _02;
+    public byte _03, _04, _05;
+    public byte _06, _07, _08, _09;
+    public byte _10, _11, _12, _13, _14;
+};
+[System.Serializable]
+public struct TetDepthMap
+{
+    public TetDepth _0, _1, _2, _3;
+};
 
 public class InfProbeGen : MonoBehaviour
 {
@@ -15,6 +29,7 @@ public class InfProbeGen : MonoBehaviour
 
     public Vector3[] vProbes;
     public TetIndex[] vTetIndices;
+    public TetDepthMap[] vTetDepthMap;
 
     private void Awake()
     {
--- Assets/InfProbe/InfProbeGenInspector.cs
+++ Assets/InfProbe/InfProbeGenInspector.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEditor;
@@ -8,10 +9,7 @@ using UnityEngine.Rendering;
 public class InfProbeGenInspector : Editor
 {
     private InfProbeGen probeGen;
-
-    private Vector3 vOldAABBOrigin = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-    private Vector3 vOldAABBExtents = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
-    private Vector3 vOldPorbeSpacing = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+    private List<MeshFilter> meshList = new List<MeshFilter>();
 
 
     [DllImport("tetgen_x64.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -21,6 +19,36 @@ public class InfProbeGenInspector : Editor
     [DllImport("tetgen_x64.dll", CallingConvention = CallingConvention.Cdecl)]
     private static extern void TGGetTetIndices(uint[] vOut);
 
+
+    private void _findAllMeshes()
+    {
+        meshList.Clear();
+
+        foreach (var gObj in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (!gObj.activeSelf)
+                continue;
+
+            if (!EditorUtility.IsPersistent(gObj.transform.root.gameObject) && !(gObj.hideFlags == HideFlags.NotEditable || gObj.hideFlags == HideFlags.HideAndDontSave))
+            {
+                //foreach(var mesh in gObj.GetComponents<MeshFilter>())
+                //{
+                //    if ((mesh.GetComponents<MeshRenderer>().Length + mesh.GetComponentsInChildren<MeshRenderer>().Length) <= 0)
+                //        continue;
+
+                //    meshList.Add(mesh);
+                //}
+
+                foreach (var mesh in gObj.GetComponentsInChildren<MeshFilter>(false))
+                {
+                    if ((mesh.GetComponents<MeshRenderer>().Length + mesh.GetComponentsInChildren<MeshRenderer>().Length) <= 0)
+                        continue;
+
+                    meshList.Add(mesh);
+                }
+            }
+        }
+    }
 
     private void _generateTets()
     {
@@ -59,6 +87,7 @@ public class InfProbeGenInspector : Editor
 
             iLen >>= 2;
             probeGen.vTetIndices = new TetIndex[iLen];
+            probeGen.vTetDepthMap = new TetDepthMap[iLen];
 
             for (int i = 0; i < iLen; ++i)
             {
@@ -70,40 +99,169 @@ public class InfProbeGenInspector : Editor
         }
     }
 
-    private void _fillTriInfo(Vector3 vOrigin, Vector3 v0, Vector3 v1, Vector3 v2)
+    private static ref byte _depthToIndex(ref TetDepth tetDepth, int iID)
     {
+        switch (iID)
+        {
+            case 0:
+                return ref tetDepth._00;
+            case 1:
+                return ref tetDepth._01;
+            case 2:
+                return ref tetDepth._02;
+            case 3:
+                return ref tetDepth._03;
+            case 4:
+                return ref tetDepth._04;
+            case 5:
+                return ref tetDepth._05;
+            case 6:
+                return ref tetDepth._06;
+            case 7:
+                return ref tetDepth._07;
+            case 8:
+                return ref tetDepth._08;
+            case 9:
+                return ref tetDepth._09;
+            case 10:
+                return ref tetDepth._10;
+            case 11:
+                return ref tetDepth._11;
+            case 12:
+                return ref tetDepth._12;
+            case 13:
+                return ref tetDepth._13;
+            case 14:
+                return ref tetDepth._14;
+        }
+        return ref tetDepth._14;
+    }
+    private static bool _rayTriIntersect(Ray ray, Vector3 v0, Vector3 v1, Vector3 v2, ref float t)
+    {
+        const float kEpsilon = 0.000001f;
 
+        // edges from v1 & v2 to v0.     
+        Vector3 e1 = v1 - v0;
+        Vector3 e2 = v2 - v0;
+
+        Vector3 h = Vector3.Cross(ray.direction, e2);
+        float a = Vector3.Dot(e1, h);
+        if ((a > -kEpsilon) && (a < kEpsilon))
+            return false;
+
+        float f = 1.0f / a;
+
+        Vector3 s = ray.origin - v0;
+        float u = f * Vector3.Dot(s, h);
+        if ((u < 0.0f) || (u > 1.0f))
+            return false;
+
+        Vector3 q = Vector3.Cross(s, e1);
+        float v = f * Vector3.Dot(ray.direction, q);
+        if ((v < 0.0f) || (u + v > 1.0f))
+            return false;
+
+        float _t = f * Vector3.Dot(e2, q);
+        if (_t > kEpsilon)
+        {
+            t = _t;
+            return true;
+        }
+        else
+            return false;
+
+        return false;
+    }
+    private void _fillTriInfo(ref TetDepth tetDepth, Vector3 vOrigin, Vector3 v0, Vector3 v1, Vector3 v2)
+    {
+        int iID = 0;
+
+        for (int i = 0; i <= 4; ++i)
+        {
+            var vP = Vector3.Lerp(v0, v1, i * 0.25f);
+            var vQ = Vector3.Lerp(v0, v2, i * 0.25f);
+
+            for (int j = 0; j <= i; ++j)
+            {
+                var vTarget = Vector3.Lerp(vP, vQ, j * 0.25f);
+
+                var vDiff = vTarget - vOrigin;
+                var fLongest = vDiff.magnitude;
+                if (fLongest <= 0.0001f)
+                {
+                    _depthToIndex(ref tetDepth, iID++) = 0xff;
+                }
+                else {
+                    var vToward = vDiff / fLongest;
+                    var rToward = new Ray(vOrigin, vToward);
+
+                    var fShortest = fLongest;
+                    foreach (var filter in meshList)
+                    {
+                        var bAABB = filter.sharedMesh.bounds;
+                        var vAABBMin = filter.transform.TransformVector(bAABB.min);
+                        var vAABBMax = filter.transform.TransformVector(bAABB.max);
+                        bAABB.SetMinMax(vAABBMin, vAABBMax);
+                        if (!bAABB.IntersectRay(rToward))
+                            continue;
+            
+                        for (int iSubMesh = 0; iSubMesh < filter.sharedMesh.subMeshCount; ++iSubMesh)
+                        {
+                            var indices = filter.sharedMesh.GetIndices(iSubMesh);
+                            for (int iIndex = 0; iIndex < indices.Length; iIndex += 3)
+                            {
+                                var fDist = fLongest;
+                                if(_rayTriIntersect(
+                                    rToward,
+                                    filter.transform.TransformVector(filter.sharedMesh.vertices[indices[iIndex + 0]]),
+                                    filter.transform.TransformVector(filter.sharedMesh.vertices[indices[iIndex + 1]]),
+                                    filter.transform.TransformVector(filter.sharedMesh.vertices[indices[iIndex + 2]]),
+                                    ref fDist
+                                    ))
+                                {
+                                    fShortest = Mathf.Min(fShortest, fDist);
+                                }
+                            }
+                        }
+                    }
+
+                    fShortest /= fLongest;
+                    fShortest *= 255.0f;
+                    fShortest = Mathf.Clamp(fShortest, 0.0f, 255.0f);
+                    _depthToIndex(ref tetDepth, iID++) = (byte)fShortest;
+                }
+            }
+        }
     }
     private void _fillDepthInfo()
     {
-        foreach (var iTet in probeGen.vTetIndices)
+        for (int i = 0, e = probeGen.vTetIndices.Length; i < e; ++i)
         {
+            var iTet = probeGen.vTetIndices[i];
+            var iTetDepth = probeGen.vTetDepthMap[i];
+
             { // 0 -> 1 2 3
-                _fillTriInfo(probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._3]);
+                _fillTriInfo(ref iTetDepth._0, probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._3]);
             }
             { // 1 -> 0 2 3
-                _fillTriInfo(probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._3]);
+                _fillTriInfo(ref iTetDepth._1, probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._3]);
             }
             { // 2 -> 0 1 3
-                _fillTriInfo(probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._3]);
+                _fillTriInfo(ref iTetDepth._2, probeGen.vProbes[iTet._2], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._3]);
             }
             { // 3 -> 0 1 2
-                _fillTriInfo(probeGen.vProbes[iTet._3], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._2]);
+                _fillTriInfo(ref iTetDepth._3, probeGen.vProbes[iTet._3], probeGen.vProbes[iTet._0], probeGen.vProbes[iTet._1], probeGen.vProbes[iTet._2]);
             }
         }
     }
 
     private void _rebuildProbes()
     {
-        if (vOldAABBOrigin != probeGen.transform.position || vOldAABBExtents != probeGen.vAABBExtents || vOldPorbeSpacing != probeGen.vProbeSpacing)
-        {
-            _generateTets();
-            _fillDepthInfo();
+        _generateTets();
 
-            vOldAABBOrigin = probeGen.transform.position;
-            vOldAABBExtents = probeGen.vAABBExtents;
-            vOldPorbeSpacing = probeGen.vProbeSpacing;
-        }
+        _findAllMeshes();
+
+        _fillDepthInfo();
     }
 
 
@@ -114,13 +272,14 @@ public class InfProbeGenInspector : Editor
 
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Rebuild Probes"))
+            _rebuildProbes();
     }
 
     private void OnSceneGUI()
     {
-        _rebuildProbes();
-
         Handles.zTest = CompareFunction.Less;
 
         { // render Tets
